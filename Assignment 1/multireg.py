c0b18b8031409179b827497138b18145c1ba4b56
adityadas8888 <aditya.das8.ad@gmail.com>
2019-09-10 01:02:07 -0500

    finished

--- Assignment 1/multireg.py
+++ Assignment 1/multireg.py
@@ -2,28 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-pd.set_option('display.max_rows', None);
 
+#read_replace function. This function is used to read the dataset from the folder and replace the Nominal values with some quantitative values.
 def read_replace():
     dir_path = os.path.dirname(os.path.realpath(__file__));
     data = pd.read_csv(dir_path+"/IRIS.csv");
     data.species.replace(['Iris-setosa', 'Iris-versicolor','Iris-virginica'], [1, 2, 3], inplace=True);
     return data;
 
-
-def kfold(data,k=3):
+#kfold function. This function is used to implement the k-fold cross validation. It calls the train_model and test_model functions, finds the average of the accuracies and prints it to the user.
+def kfold(data,k):
     chunks = chunkify(data, k);
     acc = [];
     for i in range(k):
         test_data = chunks[i];
         train_data = data.drop(test_data.index);
-        betacap = train_model(train_data);
-        # accuracy = 
-        print(i);
-        test_model(betacap,test_data);
-        # acc.append(accuracy);
-    # print('Accuracy for a multivariate regression with {} folds is {}'.format(k,np.mean(acc))); 
+        betacap = train_model(train_data);  
+        accuracy = test_model(betacap,test_data);
+        acc.append(accuracy);
+    print('Accuracy for a multivariate regression with {} folds is {}%'.format(k,(np.mean(acc)*100))); 
 
+# train_model function. This function is used to train the model. It returns the coeffecients of the trained multivariate regression model.
 def train_model(train_data):
     data = train_data
     train_data = data.drop('species',axis = 1);
@@ -31,42 +30,24 @@ def train_model(train_data):
     betacap = np.linalg.inv((train_data.transpose().dot(train_data))).dot(train_data.transpose().dot(data.species));
     return betacap;
 
+#test_model function. This function is used to check the accuracy of the trained model. 
 def test_model(betacap,test_data):
     foo=[];
     predicted =(test_data.iloc[:,0:4].values*betacap);
     for i in range(len(predicted)):
         foo.append(round(sum(predicted[i])));
-    test_data['predicted']=foo;
-    test_data['correct'] = np.where(test_data['species']==test_data['predicted'], 1, 0);
+    test_data.loc[:,'predicted']=foo;
+    test_data.loc[:,'correct'] = np.where(test_data['species']==test_data['predicted'], 1, 0);
     accuracy=test_data['correct'].sum()/len(test_data);
     return accuracy;
-
+    
+#chunkify function. Divides a particular pandas dataframe into chunks of n.
 def chunkify(lst,n):
     return [ lst[i::n] for i in range(n) ]
 
+# main function. You can change the k value to change the k fold cross validation
 def main():
   data = read_replace();
-  kfold(data,k=15);  
+  kfold(data,k=5);  
 if __name__== "__main__":
   main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   # train_set = data.sample(frac=0.75, random_state=0)
-    # test_set = data.drop(train_set.index).sample(frac =1.0);
-    # 
-   
