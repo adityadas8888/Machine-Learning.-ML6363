79e0f017e186f6a7a4e549e339738893a32c46cb
adityadas8888 <aditya.das8.ad@gmail.com>
2019-09-09 19:10:39 -0500

    got the coeffecients

--- Assignment 1/multireg.py
+++ Assignment 1/multireg.py
@@ -4,87 +4,15 @@ import pandas as pd
 import copy
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__));
-data = pd.read_csv(dir_path+"/IRIS.csv");
-print(data);
-# def center_assignment(df, centers):
-#     for i in centers.keys():
-#         df['distance_from_{}'.format(i)] = (
-#             np.sqrt(
-#                 (df['x'] - centers[i][0]) ** 2
-#                 + (df['y'] - centers[i][1]) ** 2
-#             )
-#         )
-#     centroid_distance_cols = ['distance_from_{}'.format(i) for i in centers.keys()]
-#     df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
-#     df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
-#     df['color'] = df['closest'].map(lambda x: colmap[x])
-#     return df
-
-# def update_center(center):
-#     for i in centers.keys():
-#         centers[i][0] = np.mean(df[df['closest'] == i]['x'])
-#         centers[i][1] = np.mean(df[df['closest'] == i]['y'])
-#     return center
-
-
-# iter = 1 
-# mean1 = [1,0]
-# mean2 = [0,1.5]
-
-# Sigma1 = [[0.9,0.4],[0.4,0.9] ]
-# Sigma2 = [[0.9,0.4],[0.4,0.9] ]
-# #c=[[10,10],[-10,-10]]                                                          #centers for Part 2 of Question 1
-# c=[[10,10],[-10,-10],[10,-10],[-10,10]]                                         #centers for Part 3 of Question 1
-# #k=2                                                                            # cluster for Part 2 of Question 1
-# k=4                                                                             # cluster for Part 3 of Question 1
-# colmap = {1: 'r', 2: 'g', 3: 'b',4:'y',5:'pink',6:'brown'}
-
-# x, y = np.random.multivariate_normal(mean1, Sigma1, 500).T
-# p, q = np.random.multivariate_normal(mean2, Sigma2, 500).T
-
-# x=np.concatenate((x, p))
-# y=np.concatenate((y, q))
-
-# d = {
-#     'x': x,
-#     'y': y
-# }
-# df=pd.DataFrame(d)
-
-# centers = {
-#      i+1: [c[i][0],c[i][1]]
-#      for i in range(len(c))
-#  }
-
-# df = center_assignment(df, centers)
-# dx=1
-# dy=1
-
-# while iter<=10000 or (dx>=0.001 and dy>= 0.001):
-   
-#     closest_centers = df['closest'].copy(deep=True)
-#     old_centers = copy.deepcopy(centers)
-   
-#     for i in old_centers.keys():
-#         old_x = old_centers[i][0]
-#         old_y = old_centers[i][1]
-#         dx = (centers[i][0] - old_centers[i][0])
-#         dy = (centers[i][1] - old_centers[i][1])
-
-#     centers = update_center(centers)
-#     df = center_assignment(df, centers)
-#     iter=iter+1
-#     if closest_centers.equals(df['closest']):
-#         break
-
-# fig = plt.figure(figsize=(10, 10))
-# plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k',marker="x")
-# for i in centers.keys():
-#     plt.scatter(*centers[i], color='black')
-# plt.xlim(-3, 5)
-# plt.ylim(-3, 5)
-# print(centers)
-# print(iter)
-# print(df)
-# plt.show()
+def read_rand():
+    dir_path = os.path.dirname(os.path.realpath(__file__));
+    data = pd.read_csv(dir_path+"/IRIS.csv");
+    data.species.replace(['Iris-setosa', 'Iris-versicolor','Iris-virginica'], [1, 2, 3], inplace=True);
+    train_set = data.sample(frac=0.75, random_state=0)
+    test_set = data.drop(train_set.index).sample(frac =1.0);
+    data2 = data.drop('species',axis = 1);
+    data2= data2.values;
+    betacap = np.linalg.inv((data2.transpose().dot(data2))).dot(data2.transpose().dot(data.species));
+    print(betacap);
+    print(data);
+read_rand();
