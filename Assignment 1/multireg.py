d44c08c1e28107996d1bba794dd4cd7761cc9a15
adityadas8888 <aditya.das8.ad@gmail.com>
2019-09-10 01:08:07 -0500

    added k checks and randomizer

--- Assignment 1/multireg.py
+++ Assignment 1/multireg.py
@@ -8,20 +8,24 @@ def read_replace():
     dir_path = os.path.dirname(os.path.realpath(__file__));
     data = pd.read_csv(dir_path+"/IRIS.csv");
     data.species.replace(['Iris-setosa', 'Iris-versicolor','Iris-virginica'], [1, 2, 3], inplace=True);
+    data = data.sample(frac=1).reset_index(drop=True);
     return data;
 
 #kfold function. This function is used to implement the k-fold cross validation. It calls the train_model and test_model functions, finds the average of the accuracies and prints it to the user.
 def kfold(data,k):
-    chunks = chunkify(data, k);
-    acc = [];
-    for i in range(k):
-        test_data = chunks[i];
-        train_data = data.drop(test_data.index);
-        betacap = train_model(train_data);  
-        accuracy = test_model(betacap,test_data);
-        acc.append(accuracy);
-    print('Accuracy for a multivariate regression with {} folds is {}%'.format(k,(np.mean(acc)*100))); 
-
+    if k>1 and k<len(data):
+        chunks = chunkify(data, k);
+        acc = [];
+        for i in range(k):
+            test_data = chunks[i];
+            train_data = data.drop(test_data.index);
+            betacap = train_model(train_data);  
+            accuracy = test_model(betacap,test_data);
+            acc.append(accuracy);
+        print('Accuracy for a multivariate regression with {} folds is {}%'.format(k,(np.mean(acc)*100))); 
+    else:
+        print('The entered k value is invalid. Exiting!!!');
+        exit;
 # train_model function. This function is used to train the model. It returns the coeffecients of the trained multivariate regression model.
 def train_model(train_data):
     data = train_data
@@ -40,7 +44,7 @@ def test_model(betacap,test_data):
     test_data.loc[:,'correct'] = np.where(test_data['species']==test_data['predicted'], 1, 0);
     accuracy=test_data['correct'].sum()/len(test_data);
     return accuracy;
-    
+
 #chunkify function. Divides a particular pandas dataframe into chunks of n.
 def chunkify(lst,n):
     return [ lst[i::n] for i in range(n) ]
@@ -48,6 +52,6 @@ def chunkify(lst,n):
 # main function. You can change the k value to change the k fold cross validation
 def main():
   data = read_replace();
-  kfold(data,k=5);  
+  kfold(data,k=50);  
 if __name__== "__main__":
   main()
