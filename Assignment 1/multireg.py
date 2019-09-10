06986f0ae29288f4dccafddeb757bdb894c4160e
adityadas8888 <aditya.das8.ad@gmail.com>
2019-09-10 00:38:05 -0500

    nearly done

--- Assignment 1/multireg.py
+++ Assignment 1/multireg.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import copy
 import os
+pd.set_option('display.max_rows', None);
 
 def read_replace():
     dir_path = os.path.dirname(os.path.realpath(__file__));
@@ -18,56 +18,55 @@ def kfold(data,k=3):
         test_data = chunks[i];
         train_data = data.drop(test_data.index);
         betacap = train_model(train_data);
-        accuracy = test_model(betacap,test_data);
-        acc.append(accuracy);
-    print('Accuracy for a multivariate regression with {} folds is {}'.format(k,np.mean(acc))); 
+        # accuracy = 
+        print(i);
+        test_model(betacap,test_data);
+        # acc.append(accuracy);
+    # print('Accuracy for a multivariate regression with {} folds is {}'.format(k,np.mean(acc))); 
 
 def train_model(train_data):
-    train_data = train_data.values;
-    betacap = np.linalg.inv((train_data.transpose().dot(train_data))).dot(train_data.transpose().dot(train_data.species));
+    data = train_data
+    train_data = data.drop('species',axis = 1);
+    train_data= train_data.values;
+    betacap = np.linalg.inv((train_data.transpose().dot(train_data))).dot(train_data.transpose().dot(data.species));
     return betacap;
 
 def test_model(betacap,test_data):
-    
+    foo=[];
+    predicted =(test_data.iloc[:,0:4].values*betacap);
+    for i in range(len(predicted)):
+        foo.append(round(sum(predicted[i])));
+    test_data['predicted']=foo;
+    test_data['correct'] = np.where(test_data['species']==test_data['predicted'], 1, 0);
+    accuracy=test_data['correct'].sum()/len(test_data);
+    return accuracy;
 
-
-    # train_set = data.sample(frac=0.75, random_state=0)
-    # test_set = data.drop(train_set.index).sample(frac =1.0);
-    # data2 = data.drop('species',axis = 1);
-    # data2= data2.values;
-    # betacap = np.linalg.inv((data2.transpose().dot(data2))).dot(data2.transpose().dot(data.species));
-    # # print(betacap);
-    # print(data);
-    
 def chunkify(lst,n):
     return [ lst[i::n] for i in range(n) ]
-# def kfold(k=10):
-#     bins = []
-#     a = []
-#     #random.shuffle(data)
-#     k = len(data)//7
-#     for i in range(0, len(data)):
-#         a.append(data[i])
-#         if len(a) % k == 0:
-#             bins.append(a)
-#             a = []
-#     bins.append(a)
-#     efficiency = 0
-#     for i in range(0, len(bins)):
-#         testing_set = bins[i]
-#         training_set = []
-#         for j in range(0, len(bins)):
-#             if i is not j:
-#                 for b in bins[j]:
-#                     training_set.append(b)
-#         tree = id3(training_set, target_attribute, attributes)
-#         acccr = (accuracy(testing_set, tree)[1])
-#         efficiency += acccr
-#         print("Bin", i, " accuracy =", acccr)
-#     print("Average accuracy =", efficiency / len(bins))
 
 def main():
   data = read_replace();
-  kfold(data,k=10);  
+  kfold(data,k=15);  
 if __name__== "__main__":
   main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   # train_set = data.sample(frac=0.75, random_state=0)
+    # test_set = data.drop(train_set.index).sample(frac =1.0);
+    # 
+   
