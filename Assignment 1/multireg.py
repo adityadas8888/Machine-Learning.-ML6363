a655464fc35f4fa6f576801e93999f369ae9d880
adityadas8888 <aditya.das8.ad@gmail.com>
2019-09-10 01:13:15 -0500

    added comments

--- Assignment 1/multireg.py
+++ Assignment 1/multireg.py
@@ -5,20 +5,20 @@ import os
 
 #read_replace function. This function is used to read the dataset from the folder and replace the Nominal values with some quantitative values.
 def read_replace():
-    dir_path = os.path.dirname(os.path.realpath(__file__));
-    data = pd.read_csv(dir_path+"/IRIS.csv");
-    data.species.replace(['Iris-setosa', 'Iris-versicolor','Iris-virginica'], [1, 2, 3], inplace=True);
-    data = data.sample(frac=1).reset_index(drop=True);
+    dir_path = os.path.dirname(os.path.realpath(__file__));                 
+    data = pd.read_csv(dir_path+"/IRIS.csv");                                                          # reads the dataset from the current working directory     
+    data.species.replace(['Iris-setosa', 'Iris-versicolor','Iris-virginica'], [1, 2, 3], inplace=True);  # replacing the nominal values
+    data = data.sample(frac=1).reset_index(drop=True);                                                  # randomizing the dataset and resetting the index
     return data;
 
 #kfold function. This function is used to implement the k-fold cross validation. It calls the train_model and test_model functions, finds the average of the accuracies and prints it to the user.
 def kfold(data,k):
-    if k>1 and k<len(data):
-        chunks = chunkify(data, k);
+    if k>1 and k<len(data):                                                                             # checking if k is valid, else exit
+        chunks = chunkify(data, k);                                                                     # dividing the dataset into chunks    
         acc = [];
         for i in range(k):
             test_data = chunks[i];
-            train_data = data.drop(test_data.index);
+            train_data = data.drop(test_data.index);                                                    # separating the test and training dataset
             betacap = train_model(train_data);  
             accuracy = test_model(betacap,test_data);
             acc.append(accuracy);
@@ -31,13 +31,13 @@ def train_model(train_data):
     data = train_data
     train_data = data.drop('species',axis = 1);
     train_data= train_data.values;
-    betacap = np.linalg.inv((train_data.transpose().dot(train_data))).dot(train_data.transpose().dot(data.species));
+    betacap = np.linalg.inv((train_data.transpose().dot(train_data))).dot(train_data.transpose().dot(data.species));             # beta = inverse((transp(X).X)).(transp(X).Y)
     return betacap;
 
 #test_model function. This function is used to check the accuracy of the trained model. 
 def test_model(betacap,test_data):
     foo=[];
-    predicted =(test_data.iloc[:,0:4].values*betacap);
+    predicted =(test_data.iloc[:,0:4].values*betacap);                                                                          # multiplying the coeffecients with the test data to get the predicted values
     for i in range(len(predicted)):
         foo.append(round(sum(predicted[i])));
     test_data.loc[:,'predicted']=foo;
@@ -52,6 +52,6 @@ def chunkify(lst,n):
 # main function. You can change the k value to change the k fold cross validation
 def main():
   data = read_replace();
-  kfold(data,k=50);  
+  kfold(data,k=5);  
 if __name__== "__main__":
   main()
