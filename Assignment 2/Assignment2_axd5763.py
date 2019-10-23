a06f329e609499ed1d8b12aa8a24eac29e8f17bf
adityadas8888 <aditya.das8.ad@gmail.com>
2019-10-22 23:24:27 -0500

    Final file

--- Assignment 2/Assignment2_axd5763.py
+++ Assignment 2/Assignment2_axd5763.py
@@ -6,20 +6,16 @@ import re
 import timeit
 
 sub_folders=[]
-file_name = []
-path=[]
-stop_words = []      
-global group
-group='NULL'
-cwd = os.getcwd()
-path = cwd+"/20_newsgroups/"
-total_dic = {}                                  
+path=[]      
+global_dictionary = {}                                  
 dictionary = {}
 file_name ={}
-
+global domain
+domain='NULL'
+path = os.getcwd()+"/20_newsgroups/"
 
 def stop_words_calc():
-    stop_words=[]
+    pathcheck('stopwords.txt','Stop Words file');
     f=open("stopwords.txt", "r");
     next_line=['\n'];
     if f.mode == 'r':
@@ -27,118 +23,127 @@ def stop_words_calc():
         for i in next_line:
             stop_words= stop_words.replace(i,' ')
             li = list(stop_words.split(" "))
-    data = ([ f' {x} ' for x in li ]);
-    data.pop()
-    return data;
+    text = ([ f' {x} ' for x in li ]);
+    text.pop();
+    return text;
 
+def load_test_files(sub_folders):               
+    global domain                                 
+    while (len(sub_folders)):
+        random_group = random.randint(0,len(sub_folders)-1)
+        random_newsgroup = sub_folders[random_group]
+        if len(file_name[random_newsgroup])== 0:
+            sub_folders.remove(random_newsgroup)
+        else:
+            random_file_num = random.randint(0, len(file_name[random_newsgroup])-1)
+            random_file = file_name[random_newsgroup][random_file_num]
+            file_name[random_newsgroup].remove(random_file)
+            domain = random_newsgroup
+            text = open_file(path + random_newsgroup + '/'+ random_file);
+            return text
+    domain = 'NULL'
+    return domain
 
-def text_preprocessing(text):                                                                   ### cleaned up
+def preprocessing(text,stop_words):                                                                
     symbol_remove = ['<','>','?','.','"',')','(','|','-','#','*','+','\'','&','^','`','~','\t','$','%',"'",'!','/','\\','=',',',':']
-    text = text.replace('\n', ' ')
     text = text.lower()
     email = re.findall(r'[\w\.-]+@[\w\.-]+', text)              ## regex email addresses and removing them
+    text = text.replace('\n', ' ')
     for word in email:
         text = text.replace(word,' ')
-    for word in stop_words:
-        text = text.replace(word,' ');
+    for word in stop_words:                         #   Comment this line and the line below to 
+        text = text.replace(word,' ');              #   not remove stop words from the dataset
     for word in symbol_remove:
         text = text.replace(word,'')
+    text = text.split(' ');
+    if '' in text: text.remove('');
+    if ' ' in text: text.remove(' ');
     return text
-
-def load_test_files(sub_folders):               ## change this to do just last 50%
-    global group                                 
-    while (len(sub_folders)):
-        r_fo = random.randint(0,len(sub_folders)-1)
-        sub_foldern = sub_folders[r_fo]
-        if len(file_name[sub_foldern])== 0:
-            sub_folders.remove(sub_foldern)
-        else:
-            r_fi = random.randint(0, len(file_name[sub_foldern])-1)
-            fil = file_name[sub_foldern][r_fi]
-            file_name[sub_foldern].remove(fil)
-            group = sub_foldern
-            data = open_file(path + sub_foldern + '/'+ fil);
-            return data
-    group = 'NULL'
-    return 'NULL'
     
-def get_probability(fields, dic):                                   ## understand and change this
-    sum_ = sum(dic.values())
+def calculate(_file_, dictionary,sub_folder_list):                                   
     probability = 0.0
-    for f in fields:
-        value = dic.get(f, 0.0) + 0.0001
-        probability = probability + math.log(float(value)/float(sum_))
+    predictor_prob = sum(dictionary.values())
+    prior_probability=1/len(sub_folder_list)
+    for word in _file_:
+        likelihood = dictionary.get(word, 0.0000000001)                                  ### change the precision here to increase or decrease accuracy by 2%( range 0.1 to 0.000001)
+        probability+= math.log((float(likelihood)*prior_probability)/float(predictor_prob))
     return probability
 
 
 def open_file(file_location):
     with open(file_location, encoding="latin-1") as datafile:
-        data = datafile.read();
-    return data
+        text = datafile.read();
+    return text
 
 
-def train_model(sub_folderlist):
+def train_model(sub_folder_list,stop_words):               
     print ("...............................Training the model...............................\n")
-    for fo in sub_folderlist:
-        # print('.')
-        dic = {}
-        sub_folder = path + fo
-        files = os.listdir(sub_folder)
-        training_set =int(len(files)/2)+1      ## half the dataset constituents the training files.
-        flag = 0
-        for fi in files:
-            flag+= 1
+    i=1;
+    record=[]
+    for sub_folder in sub_folder_list:
+        print(i/len(sub_folder_list)*100,"% trained");
+        i+=1;
+        local_dictionary = {};
+        sub_folder_path = path + sub_folder;
+        files = os.listdir(sub_folder_path);
+        training_set =int(len(files)/2)+1;      # half the dataset constituents the training files.
+        flag = 0;
+        for _file_ in files:
+            flag+= 1;
             if flag > training_set:
-                break
-            opened_file=open_file(sub_folder + '/'+fi);
-            data = text_preprocessing(opened_file);
-            fields = data.split(' ');
-            if '' in fields: fields.remove('');
-            if ' ' in fields: fields.remove(' ');
-            for field in fields:
-                # if field == ' ' or field == '':
-                #     continue
-                value = dic.get(field, 0)
-                value_t = total_dic.get(field, 0)
-                dic[field]=value+1;                     ## laplacian smoothening by default
-                total_dic[field]=value_t+1
-            files.remove(fi)
-        file_name[fo] = files
-        dictionary[fo] = dic
-    print (len(total_dic), 'unique words were discovered during training.\n');
+                break;
+            opened_file=open_file(sub_folder_path + '/'+_file_);                            # The dataset needed to be opened in Latin-1 Encoding.
+            words=preprocessing(opened_file,stop_words);
+            for field in words:
+                value = local_dictionary.get(field, 0);
+                value_t = global_dictionary.get(field, 0);
+                local_dictionary[field]=value+1;                                            # laplacian smoothening
+                global_dictionary[field]=value_t+1;
+            record.append(_file_);
+        files=list(set(files).difference(record));                  ## comment this line to test the entire dataset.
+        dictionary[sub_folder] = local_dictionary;
+        file_name[sub_folder] = files;
 
-def test_model(sub_folderlist):
-    print ("\n...............................Testing the model...............................")
-    data = 1
-    sub_folders = copy.deepcopy(sub_folderlist)
-    iteration = 0
-    success = 0
-    while (data):
-        data = load_test_files(sub_folders)                            ## loadin the test files
-        iteration = iteration + 1
-        if data =='NULL':
-            break
-        data = text_preprocessing(data)
-        fields = data.split(' ')
-        if '' in fields: fields.remove('')
-        if ' ' in fields: fields.remove(' ')
-        probabilities = []
-        for c in sub_folderlist:
-            probabilities.append(get_probability(fields,dictionary[c]))
-        if group == sub_folderlist[probabilities.index(max(probabilities))]:
-            success = success + 1   
-    return success,iteration
+def test_model(sub_folder_list,stop_words):
+    print ("\n................................Testing the model................................\n")
+    loop = 0
+    text = 1
+    prediction = 0
+    sub_folders = copy.deepcopy(sub_folder_list)
+    while (text):
+        if(loop>0 and loop%493==0):
+            print(int(loop/9966*100),'% tested');
+        text = load_test_files(sub_folders);                            ## loadin the test files
+        if text =='NULL':
+            break;
+        loop+= 1;
+        confusion_matrix = [];
+        words=preprocessing(text,stop_words);
+        for sub_folder in sub_folder_list:
+            confusion_matrix.append(calculate(words,dictionary[sub_folder],sub_folder_list));
+        predicted=max(confusion_matrix);
+        if domain == sub_folder_list[confusion_matrix.index(predicted)]:
+            prediction +=1;   
+    return prediction,loop-1
+
+def pathcheck(path,supporting_file_name):
+    if os.path.exists(path):
+        print('\n',supporting_file_name,"detected in the current Directory\n");
+    else:
+        print('\nDataset not present in parent directory. Load dataset and try again.\n Program exiting!!!');
+        exit();
 
 def main():
-    start = timeit.default_timer()
-    sub_folderlist = os.listdir(path)                  ## list of all sub directories
-    stop_words = stop_words_calc()
-    group='NULL'
-    train_model(sub_folderlist);
-    success,iteration= test_model(sub_folderlist);
-    print ('Success rate = %.1f'% (float(success)/float(iteration - 1)*100))
-    stop = timeit.default_timer()
-    print('Time: ', stop - start) 
+    stop_words= stop_words_calc()
+    start = timeit.default_timer();
+    pathcheck(path,'20 News Group Dataset');
+    sub_folder_list = os.listdir(path);                  ## list of all sub directories
+    domain='NULL';
+    train_model(sub_folder_list,stop_words);
+    prediction,loop= test_model(sub_folder_list,stop_words);
+    print ('Prediction accuracy = ',(prediction/loop*100),'%' );
+    stop = timeit.default_timer();
+    print('Time elapsed: ', int(stop - start),"secs"); 
 if __name__== "__main__":
   main()
 
---
+++ Assignment 2/test.py
@@ -0,0 +1,3 @@
+import random
+import math
+print(math.log(20));
